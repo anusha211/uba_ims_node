57a58bce32f11672c0b9156581a4f795b8276fa2
anusha211 <anushachalise211@gmail.com>
2024-06-14 12:01:42 +0545

    without the use of commander module

--- commands/create.js
+++ commands/create.js
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { promises as fs } from 'fs';
 
-
 async function readDatabase() {
     const data = await fs.readFile('db.json', 'utf-8');
     return JSON.parse(data);
@@ -11,10 +10,11 @@ async function writeDatabase(data) {
     await fs.writeFile('db.json', JSON.stringify(data, null, 2));
 }
 
-export async function createUser(fname,lname, age) {
+export async function createUser(fname, lname, age) {
     const db = await readDatabase();
     const id = uuidv4();
-    db.users.push({ id, fname, lname, age : Number(age) });
+    db.users.push({ id, fname, lname, age: Number(age) });
     await writeDatabase(db);
-    console.log(`User created with ID ${id}`);
+    console.log(`User ${fname} ${lname} created with ID ${id}`);
 }
+
--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-import { Command } from 'commander';
 import { createUser } from './commands/create.js';
 import { readUser } from './commands/read.js';
 import { updateUser } from './commands/update.js';
 import { deleteUser } from './commands/delete.js';
 
-const program = new Command();
+const [,, command, ...args] = process.argv;
 
-program
-    .command('create <fname> <lname> <age>')
-    .description('Create a new user')
-    .action((fname,lname,age)=>createUser(fname,lname,age));
-
-program
-    .command('read <id>')
-    .description('Read a user by ID')
-    .action(readUser);
-
-program
-    .command('update <id> <fname> <lname> <age>')
-    .description('Update a user by ID')
-    .action((id,fname,lname,age)=>updateUser(id,fname,lname,age));
-
-program
-    .command('delete <id>')
-    .description('Delete a user by ID')
-    .action(deleteUser);
-
-program.parse(process.argv);
+(async () => {
+    switch (command) {
+        case 'create':
+            if (args.length < 3) {
+                console.log('Usage: create <fname> <lname> <age>');
+            } else {
+                const [fname, lname, age] = args;
+                await createUser(fname, lname, age);
+            }
+            break;
+        case 'read':
+            if (args.length < 1) {
+                console.log('Usage: read <id>');
+            } else {
+                const [id] = args;
+                await readUser(id);
+            }
+            break;
+        case 'update':
+            if (args.length < 4) {
+                console.log('Usage: update <id> <fname> <lname> <age>');
+            } else {
+                const [id, fname, lname, age] = args;
+                await updateUser(id, fname, lname, age);
+            }
+            break;
+        case 'delete':
+            if (args.length < 1) {
+                console.log('Usage: delete <id>');
+            } else {
+                const [id] = args;
+                await deleteUser(id);
+            }
+            break;
+        default:
+            console.log('Unknown command. Use one of: create, read, update, delete');
+            break;
+    }
+})();
